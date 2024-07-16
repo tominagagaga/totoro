1fc216dff7287ed9f29e7b9194622976a2860426
mtominaga <m-tominaga@stock-inc.co.jp>
2024-07-16 19:09:58 +0900

    commit3

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,11 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+export async function GET(context) {
+  return rss({
+    title: '指宿のおすすめ | BLOG',
+    description: '指宿のおすすめをご紹介🌴',
+    site: context.site,
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    customData: `<language>ja-jp</language>`,
+  });
+}
